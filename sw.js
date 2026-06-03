a756ba38a90db287ec6339940b0432135f3a72d8
wjdwjdwjdwjd0128-del <wjdwjdwjdwjd0128@gmail.com>
2026-06-03 15:28:54 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'ipark-health-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
